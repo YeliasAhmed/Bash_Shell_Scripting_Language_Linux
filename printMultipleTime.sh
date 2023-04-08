52f724206b52ab3495a596ca4f0ea2c72b95c4b3
root <root@YELIASAHMED>
2023-04-08 13:31:45 +0600

    Upated bash

---
+++ printMultipleTime.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+for (( ; ; ))
+
+do
+
+echo "yelias ahmed" >> hello.txt
+
+sleep 2
+
+done
+
+<< comments
+multi 
+comment
+comment
+comments
+
---
+++ variable.sh
@@ -0,0 +1,13 @@
+#!/bin/bash  
+#System variable is defined as Upper case.
+# e.g: $BASH/$BASH_VERSION
+echo "$BASH"
+echo "$BASH_VERSION"
+echo
+#Bash User defined variable
+name=john
+id=101
+age=20
+echo "Person's name is $name"
+echo "His Id is $id"
+echo "His age is $age"
